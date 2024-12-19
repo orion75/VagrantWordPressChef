23dc375de9c3b915498d349e081fcd1c2e5af6eb
Harol A. Reina <harold_reina75@hotmail.com>
2024-12-19 17:59:31 -0500

    Add initial Apache cookbook structure with configuration and tests

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,36 @@
+# Atributos específicos de plataforma
+apache_package = case node['platform']
+when 'debian'
+  'apache2'
+when 'fedora'
+  'httpd'
+end
+
+apache_service = case node['platform']
+when 'debian'
+  'apache2'
+when 'fedora'
+  'httpd'
+end
+
+update_command = case node['platform']
+when 'debian'
+  'sudo apt update'
+when 'fedora'
+  'sudo dnf -y update'
+end
+
+# Actualizar los repositorios de paquetes
+execute "update-packages" do
+command update_command
+end
+
+# Instalar Apache
+package apache_package do
+action :install
+end
+
+# Habilitar y arrancar el servicio Apache
+service apache_service do
+action [:enable, :start]
+end

bba92097743302773cc712f572b15c49d8f03e3b
Harol A. Reina <harold_reina75@hotmail.com>
2024-12-19 17:57:42 -0500

    Remove deprecated Apache and MariaDB recipes and configurations for Debian and Fedora

--- cookbooks/wordpress_fedora/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-
-default['wordpress']['db_name'] = 'wordpress'
-default['wordpress']['db_user'] = 'wordpressuser'
-default['wordpress']['db_password'] = 'password'
-default['wordpress']['server_name'] = 'localhost'
